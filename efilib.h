5c7e1d3246c45428d26b00954bf52a663440d0d1
Jozef Nagy <schkwve@gmail.com>
2024-04-27 22:49:00 +0200

    Added global variable initialization

--- efilib.h
+++ efilib.h
@@ -2,8 +2,20 @@
 #define EFI_EFILIB_H
 
 #include "efi.h"
+#include "efi_st.h"
 
 #define EFI_ERROR(status) (((INTN)(status)) < 0)
+
+static EFI_HANDLE g_ImageHandle;
+static EFI_SYSTEM_TABLE *g_SystemTable;
+static EFI_BOOT_SERVICES *g_BootServices;
+
+static inline void EfiLibInitialize(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+	g_ImageHandle = ImageHandle;
+	g_SystemTable = SystemTable;
+	g_BootServices = SystemTable->BootServices;
+}
 
 #endif /* EFI_EFILIB_H */
 
