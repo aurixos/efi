fd21dfd54b11b05abcd866e29c6c102fc911e06c
Jozef Nagy <132941213+schkwve@users.noreply.github.com>
2024-06-06 22:02:00 +0200

    Removed global variables
    Signed-off-by: Jozef Nagy <132941213+schkwve@users.noreply.github.com>

--- efilib.h
+++ efilib.h
@@ -6,16 +6,5 @@
 
 #define EFI_ERROR(status) (((INTN)(status)) < 0)
 
-static EFI_HANDLE g_ImageHandle;
-static EFI_SYSTEM_TABLE *g_SystemTable;
-static EFI_BOOT_SERVICES *g_BootServices;
-
-static inline void EfiLibInitialize(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
-{
-	g_ImageHandle = ImageHandle;
-	g_SystemTable = SystemTable;
-	g_BootServices = SystemTable->BootServices;
-}
-
 #endif /* EFI_EFILIB_H */
 
